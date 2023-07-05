6903ec7b8735643b494d8bd71f8e0d7b6d7bd6e4
fiza028 <rfarzana70@gmail.com>
2023-07-05 22:51:52 +0600

    updated ui testing

--- app/src/androidTest/java/com/example/cardiacrecorder/MainActivityTest.java
+++ app/src/androidTest/java/com/example/cardiacrecorder/MainActivityTest.java
@@ -3,23 +3,33 @@ package com.example.cardiacrecorder;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.content.Context;
 import android.os.SystemClock;
+import android.view.View;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import static java.util.regex.Pattern.matches;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -35,7 +45,8 @@ public class MainActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
-        @Test
+
+    @Test
         public void AddDataTest(){
             SystemClock.sleep(2000);
             onView(withId(R.id.add_note_btn)).perform(click());
@@ -49,7 +60,7 @@ public class MainActivityTest {
             Espresso.pressBack();
             onView(withId(R.id.cmnt)).perform(ViewActions.typeText("none"));
 
-            Espresso.pressBack(); //hide keyboard
+            Espresso.pressBack(); //back button
 
             onView(withId(R.id.save)).perform(click());
             SystemClock.sleep(2000);
@@ -62,21 +73,27 @@ public class MainActivityTest {
 
             SystemClock.sleep(2000);
 
-            onView(withId(R.id.date)).perform(clearText()).perform(ViewActions.typeText("31/06/2022"));
+            onView(withId(R.id.date)).perform(clearText()).perform(ViewActions.typeText("11/06/2023"));
             onView(withId(R.id.time)).perform(clearText()).perform(ViewActions.typeText("11:30"));
             onView(withId(R.id.systolic)).perform(clearText()).perform(ViewActions.typeText("130"));
             onView(withId(R.id.dia)).perform(clearText()).perform(ViewActions.typeText("100"));
             Espresso.pressBack(); //Back button
             onView(withId(R.id.heart)).perform(clearText()).perform(ViewActions.typeText("60"));
-            Espresso.pressBack(); //Back button
+            Espresso.pressBack();
             onView(withId(R.id.cmnt)).perform(clearText()).perform(ViewActions.typeText("Normal"));
-            Espresso.pressBack(); //Back button
+            Espresso.pressBack();
             SystemClock.sleep(2000);
             onView(withId(R.id.save)).perform(click());
-            SystemClock.sleep(3000);
+            SystemClock.sleep(2000);
 
         }
+   @Test
+     public void MenuButtonTest()
+    {
+    SystemClock.sleep(2000);
+    onView(withId(R.id.menu)).perform(click());
 
+    }
         @Test
     public void DeleteTest(){
             SystemClock.sleep(2000);
